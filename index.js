9680a90bcff29e04d70065d495bbbdc21254e2a7
Kelley Shackelford <kelleyalexis@yahoo.com>
2023-10-16 19:17:44 -0400

    created js event

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-function addingEventListener() {
+function addingEventListener() {const input = document.getElementById('button');
+
+function clickAlert() {alert('i was clicked!');}
+
+input.addEventListener('click', clickAlert);
 }
